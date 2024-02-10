f3a3276705f7f2190f0aed7ebee5472f4175f0e2
Rodolfo Bertolino <rmbertolino@outlook.com>
2024-02-10 20:53:23 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hello-ejs
+
+![EJS](public/img/ejs.png)
+
+
 EJS Crash Course
+
+* Getting Started
+* Evaluate an injected variable
+* Iterate/Loop over an array of values
+* If/Else statements
+* Layouts
+* Partials/Components
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const PORT = 3000
+const expressLayouts = require('express-ejs-layouts')
+
+
+const app = express()
+app.use(express.static('public'))
+app.use(cors())
+
+// Set static folder
+app.use(express.static('public'));
+
+// Configurar el motor de plantillas EJS
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '..', 'views/pages'))
+
+app.get('/', (req,res) => {
+    //res.send('<h3>Hello</h3>' + '<h3>World</h3>')
+    const data = {
+        message:'hello world',
+        image_url: 'img/ejs.png',
+        api_url: '/api/resurce',
+        people: [
+            {name: 'Dave'},
+            {name: 'Sofia'},
+            {name: 'Emma'}
+        ],
+        user: {
+            name:'Rodolfo',
+            admin:true
+        },
+        menu: [
+            {
+                menu:'Home',
+                url: '/'
+            },
+            {
+                name: 'About',
+                url: ''
+            }
+
+        ]
+    }
+    
+
+    const context = { layout: '../layouts/layout', data };
+
+    res.render('index', context)
+})
+
+app.get('/about',(req,res)=>{
+    const data = { message:'About Page' }
+
+    const params = {
+        texto: 'Haz clic aquí',
+        clase: 'btn-primary',
+        url: '/accion'
+    };
+    
+    const context = { layout : '../layouts/layout', data, params };
+
+    res.render('about', context)
+})
+
+app.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`))
+
