8f74b46fbd2bf99b5de96fe75b222a68c6ff5cba
Rodolfo Bertolino <rmbertolino@outlook.com>
2024-02-12 14:01:17 -0300

    two partials

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 // Configurar el motor de plantillas EJS
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '..', 'views/pages'))
+app.set('views', path.join(__dirname, '..', 'views'))
 
 app.get('/', (req,res) => {
     //res.send('<h3>Hello</h3>' + '<h3>World</h3>')
@@ -46,9 +46,9 @@ app.get('/', (req,res) => {
     }
     
 
-    const context = { layout: '../layouts/layout', data };
+    const context = { layout: 'layouts/layout', data };
 
-    res.render('index', context)
+    res.render('pages/index', context)
 })
 
 app.get('/about',(req,res)=>{
@@ -60,10 +60,23 @@ app.get('/about',(req,res)=>{
         url: '/accion'
     };
     
-    const context = { layout : '../layouts/layout', data, params };
+    const context = { layout : 'layouts/layout', data, params };
 
-    res.render('about', context)
+    res.render('pages/about', context)
 })
+
+app.get('/two-partials', (req,res) =>{
+    const context = {
+        layout : 'layouts/layout',
+        dataPartial1: {
+            name: 'Partial1'
+        },
+        dataPartial2: {
+            name: 'Partial2'
+        }
+    }
+    res.render('pages/two-partials', context)
+} )
 
 app.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`))
 
